b19d3946352f01126661de3b22c48ef927203a24
aevitas <m4rtijn@gmail.com>
2016-02-09 09:51:25 +0100

    Add docs for IRow members

--- src/Quantic.Etl/Abstractions/IRow.cs
+++ src/Quantic.Etl/Abstractions/IRow.cs
@@ -3,18 +3,58 @@ using System.Collections.Generic;
 
 namespace Quantic.Etl.Abstractions
 {
-    public interface IRow : IEquatable<IRow>
-    {
-        IEnumerable<string> Columns { get; }
+	/// <summary>
+	///     Interface that all types representing a Row should implement.
+	/// </summary>
+	/// <seealso cref="System.IEquatable{Quantic.Etl.Abstractions.IRow}" />
+	public interface IRow : IEquatable<IRow>
+	{
+		/// <summary>
+		///     Gets all columns in this row.
+		/// </summary>
+		/// <value>
+		///     The columns.
+		/// </value>
+		IEnumerable<string> Columns { get; }
 
-        IRow AddColumn(string column, object value = null);
+		/// <summary>
+		///     Adds a column with the specified name, and optionally the specified value.
+		/// </summary>
+		/// <param name="column">The column.</param>
+		/// <param name="value">The value.</param>
+		/// <returns></returns>
+		IRow AddColumn(string column, object value = null);
 
-        IRow RemoveColumn(string column);
+		/// <summary>
+		///     Removes the column with the specified name from this row.
+		/// </summary>
+		/// <param name="column">The column.</param>
+		/// <returns></returns>
+		IRow RemoveColumn(string column);
 
-        IRow Set<T>(string column, T value);
+		/// <summary>
+		///     Sets the specified column to the specified value.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="column">The column.</param>
+		/// <param name="value">The value.</param>
+		/// <returns></returns>
+		IRow Set<T>(string column, T value);
 
-        IRow Transform<T>(string column, Func<T, T> transformFunc);
+		/// <summary>
+		///     Transforms the specified column using the specified transformation func.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="column">The column.</param>
+		/// <param name="transformFunc">The transform function.</param>
+		/// <returns></returns>
+		IRow Transform<T>(string column, Func<T, T> transformFunc);
 
-        object Get(string column);
-    }
+		/// <summary>
+		///     Gets the value of the specified column.
+		/// </summary>
+		/// <param name="column">The column.</param>
+		/// <returns></returns>
+		object Get(string column);
+	}
 }
